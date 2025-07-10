d31f9f62c634d135ebb887ca433559f8d3ba300b
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-07-10 13:27:27 +0530

    added user controller

--- src/socketServer/index.js
+++ src/socketServer/index.js
@@ -1,4 +1,6 @@
 import { Server } from "socket.io";
+import {verifyToken} from "../middlewares/auth.middleware.js";
+import { AppError } from "../utils/error.js";
 
 class SocketService{
     _io;
@@ -9,6 +11,35 @@ class SocketService{
 
     get io(){
         return this._io;
+    }
+
+    auth(){
+        const io=this._io;
+        io.use(async(socket,next)=>{
+            try {
+                const token=socket.handshake.headers.authorization.split(" ")[1];
+                const isVerified=await verifyToken(token);
+                if(!isVerified) throw new AppError("Unauthorised",401);
+                socket.userId=isVerified.userId;
+                next();
+            } catch (error) {
+               next(new AppError("Unauthorised",401)); 
+            }
+        })
+    }
+    initListeners(){
+        const io=this._io;
+        io.on("connect",(socket)=>{
+            console.log("New socket connected",socket.id);
+
+            socket.on("event:message",async(message)=>{
+                console.log("New message recieved:",message);
+            })
+
+            socket.on("disconnect",()=>{
+                console.log("Socket server disconnected",socket.id)
+            })
+        });
     }
 }
 
