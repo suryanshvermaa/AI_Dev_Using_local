3aaf2f8d4c4ca6be747cca5ed229ae952ed92181
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-07-10 11:30:52 +0530

    asynchander added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param {Function} fn - The async function to handle.
+ * @description A function to handle async functions in Express.js.
+ * @returns {import("express").RequestHandler} - A middleware function that handles the async function.
+ */
+
+const asyncHandler = (fn) => {
+	return (req, res, next) => {
+		Promise.resolve(fn(req, res, next)).catch(next);
+	};
+};
+
+export default asyncHandler;
